ce6975865f3816593504e42704c1c44986f786f9
Pradeep Arya <pradeep.arya@paytm.com>
2018-10-27 20:50:15 +0530

    user authentication, creation, login

--- src/main/java/in/co/a2t/constant/AppConstant.java
+++ src/main/java/in/co/a2t/constant/AppConstant.java
@@ -3,4 +3,9 @@ package in.co.a2t.constant;
 public interface AppConstant {
 
   String APPLICATION_USER = "application-user";
+  String SALT = "app.pwd.salt";
+  String JWT_ISS = "cryptane-iss";
+  String JWT_LOGIN_SECRET = "app.login.secret.key";
+  String JWT_SECRET_KEY = "jwtSecretKey";
+  String JWT_ISSUER_NAME = "jwtIss";
 }
--- src/main/java/in/co/a2t/constant/Message.java
+++ src/main/java/in/co/a2t/constant/Message.java
@@ -4,8 +4,10 @@ public interface Message {
 
   String SUCCESS_CODE = "200";
   String INTERNAL_SERVER_ERROR_CODE = "500";
+  String INTERNAL_SERVER_ERROR_MSG = "internal server";
   String FETCH_SUCCESS_MSG = "successfully fetched data";
   String FETCH_FAILURE_MSG = "failed to fetch data";
   String SUCCESS = "success";
   String FAILURE = "failed";
+  String BAD_REQUEST_ERROR_MSG = "bad request";
 }
